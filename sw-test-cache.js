2f2a89511347528e9abf2f9c19e55fbf220c8842
曹玮 <413206146@qq.com>
2019-02-12 17:33:35 +0800

    fix

--- sw-test-cache.js
+++ sw-test-cache.js
@@ -1,46 +1,44 @@
 var VERSION = 'v1';
 
 // 缓存
-self.addEventListener('install', function (event) {
-  console.log('sw install', event);
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(VERSION).then(function (cache) {
-      return cache.addAll([
+    caches.open(VERSION).then(cache => (
+      cache.addAll([
         './index.html',
         './static/jquery.min.js',
         './static/banner.jpg'
-      ]);
-    })
+      ])
+    ))
   );
 });
 
 // 缓存更新
-self.addEventListener('activate', function (event) {
-  console.log('sw activate', event);
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
+    caches.keys().then(cacheNames => (
+      Promise.all(
+        cacheNames.map(cacheName => {
           // 如果当前版本和缓存版本不一致
           if (cacheName !== VERSION) {
             return caches.delete(cacheName);
           }
         })
-      );
-    })
+      )
+    ))
   );
 });
 
 // 捕获请求并返回缓存数据
-self.addEventListener('fetch', function (event) {
-  event.respondWith(caches.match(event.request).catch(function () {
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).catch(() => {
     return fetch(event.request);
-  }).then(function (response) {
+  }).then(response => {
     caches.open(VERSION).then(function (cache) {
       cache.put(event.request, response);
     });
     return response.clone();
-  }).catch(function () {
+  }).catch(() => {
     return caches.match('./static/banner.jpg');
   }));
 });
